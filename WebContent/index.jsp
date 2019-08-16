c9bb2ebbe07b2fb50e34b41acbc4e44105d02309
Furkan KOC <furkankoc18@gmail.com>
2019-08-17 00:51:22 +0300

    furkankoc first commit

---
+++ WebContent/index.jsp
@@ -0,0 +1,9 @@
+<html>
+   <head>
+      <title>Hello Spring MVC</title>
+   </head>
+   
+   <body>
+      <h2>Index page</h2>
+   </body>
+</html>
---
+++ src/_01_com/furkan/controller/HelloController.java
@@ -0,0 +1,20 @@
+package _01_com.furkan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping("/controller")
+@Controller
+public class HelloController {
+
+	@RequestMapping(value="/hello",method=RequestMethod.GET)
+	public String page(ModelMap model) {
+		model.addAttribute("msj","abdullahfurkankoc.com Test");
+			
+		return "hello";
+	}
+	
+	
+}
